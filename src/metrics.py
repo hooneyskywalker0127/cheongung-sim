4d17686cc1e6e3d65917ba0185105a45c257f27d
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-05 17:22:48 +0900

    Update metrics.py

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 class Metrics:
     def __init__(self):
@@ -7,3 +7,36 @@ class Metrics:
         self.intercept_fail = {"BallisticMissile": 0, "CruiseMissile": 0, "Drone": 0}
         self.number_hit = {"BallisticMissile": 0, "CruiseMissile": 0, "Drone": 0}
         self.battle_time = 0
+
+    def record_success(self, threat_type):
+        self.intercept_success[threat_type] += 1
+        
+    def record_used_missile(self,):
+        self.used_missile += 1
+
+    def record_fail(self,threat_type):
+        self.intercept_fail[threat_type] += 1
+        
+    def record_hit(self, threat_type):
+        self.number_hit[threat_type] += 1
+
+    def record_battle_time(self, dt):
+        self.battle_time +=dt
+    
+    def save_csv(self):
+        df = pd.DataFrame({
+    "threat_type" : ["BallisticMissile", "CruiseMissile","Drone" ],
+    "success" : list(self.intercept_success.values()),
+    "fail" : list(self.self.intercept_fail.values()),
+    "number_hit" :list(self.number_hit.values()),
+    "battle_time" : [self.battle_time]
+
+
+
+
+
+
+
+
+})
+
