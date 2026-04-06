584ea279872d42b133e54a7bc9fc60b90cc92e4f
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-06 18:27:09 +0900

    Update radar.py

--- src/radar.py
+++ src/radar.py
@@ -1,11 +1,18 @@
+from threat import Threat
+import math
 
 
 class Radar:
-    def __init__(self,detection_range,detection_prob,radar_x, radar_y):
+    def __init__(self,detection_range,radar_x, radar_y):
         self.detection_range = detection_range
-        self.detection_prob = detection_prob
+        self.detection_prob = {"BallisticMissile": 0.95, "CruiseMissile": 0.75, "Drone": 0.40} 
         self.radar_x = radar_x
         self.radar_y = radar_y
     
-    def scan(self):
-        pass
+    def scan(self,threats):
+        detected = []
+        for threat in threats:
+            dx = self.radar_x - threat.x
+            dy = self.radar_y - threat.y
+            distance = math.sqrt(dx**2 + dy**2)
+
