c40e1c3c31a5aaf7cba61d96dfbb94de4d9e62b1
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-07 17:36:05 +0900

    Update engagement.py

---
+++ src/engagement.py
@@ -0,0 +1,20 @@
+from radar import Radar
+from threat import Threat
+import numpy as np
+
+
+class Engagement:
+    def __init__(self,radar,remaining_missiles):
+        self.radar = radar
+        self.remaining_missiles = remaining_missiles
+        self.missiles = []
+        
+
+    def assign_priority(self,detected_threats):
+        score = []
+        for threat in detected_threats:
+            speed_score = np.sqrt(threat.vx**2+threat.vy**2)
+            altitude_score = 1/threat.y
+            distance_score = 1/np.sqrt(threat.x**2+threat.y**2)
+            total_score = speed_score + altitude_score + distance_score
+            score.append((total_score,threat))
