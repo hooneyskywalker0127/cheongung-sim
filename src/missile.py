9b853bf4cb6f97b04dd3769d646d3bfb74647a13
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-08 14:21:44 +0900

    Update missile.py

---
+++ src/missile.py
@@ -0,0 +1,8 @@
+
+
+class InterceptMissile:
+    def __init__(self,x,y,target_x, target_y):
+        self.x = x
+        self.y = y
+        self.target_x = target_x
+        self.target_y = target_y
