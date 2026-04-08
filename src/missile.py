8fcdfc44b57b3a7e58654f624400ab06cc9adc38
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-08 14:41:52 +0900

    Update missile.py

--- src/missile.py
+++ src/missile.py
@@ -1,4 +1,4 @@
-
+import math
 
 class InterceptMissile:
     def __init__(self,x,y,target_x, target_y):
@@ -6,3 +6,13 @@ class InterceptMissile:
         self.y = y
         self.target_x = target_x
         self.target_y = target_y
+        self.speed = 300
+        distance = math.sqrt((target_x-x)**2 + (target_y-y)**2)
+        self.vx = (target_x-x) / distance * self.speed
+        self.vy = (target_y-y) / distance * self.speed
+        self.phase = 1
+
+
+    def update(self, dt):
+        self.x += self.vx*dt
+        self.y += self.vy*dt
