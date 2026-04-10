52aa9ef4871dcb890486e97939afcbbaf387f4ce
hooneyskywalker0127 <hooney0127@gmail.com>
2026-04-10 17:59:35 +0900

    Update visualizer.py

---
+++ src/visualizer.py
@@ -0,0 +1,20 @@
+
+
+class Visualizer:
+    def __init__(self,threats,missiles,radar):
+        self.size = (800,600)
+        self.threats = threats
+        self.missiles = missiles
+        self.radar = radar
+
+
+
+
+
+
+
+
+
+class ResultGraph:
+    def __init__(self):
+        pass
